11e1b206b361fb7922b48bcafa746040f9180952
hksuri <muhammad@oatmealhealth.com>
2025-02-20 02:27:20 +0000

    Modify text on display and logo position

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -30,16 +30,16 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-left_co, cent_co,last_co = st.columns(3)
-with cent_co:
+a,b,c,d,e,f,g,h,i,j = st.columns(10)
+with e:
     # Display an image related to clinical notes
     st.image("https://images.vexels.com/media/users/3/144224/isolated/preview/589394662ba164058d2ac84b4a0643b2-medical-record-table-notes.png", caption="Simplify your clinical documentation", width=150)
 
 # Title
-st.title("Clinical Note Summarizer")
+st.title("Clinical Note Generator")
 
 # Text input
-note_input = st.text_area("Paste your clinical note here:")
+note_input = st.text_area("Enter the doctor-patient dialogue here:")
 
 # Summarize button
 if st.button("Summarize"):
@@ -48,10 +48,10 @@ if st.button("Summarize"):
             input_ids = tokenizer(note_input, return_tensors="pt", truncation=True, max_length=1024).input_ids
             output = model.generate(input_ids, max_length=512, num_beams=4, early_stopping=True)
             summary = tokenizer.decode(output[0], skip_special_tokens=True)
-            st.subheader("Summary")
+            st.subheader("Clinical Note")
             st.write(summary)
     else:
-        st.warning("Please paste a note to summarize.")
+        st.warning("Please paste a dialogue to summarize.")
 
 # Footer
 st.markdown("---")
